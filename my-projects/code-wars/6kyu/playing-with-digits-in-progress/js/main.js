64cb071b4e358106621b35b5836aec02af4be8d0
Ernesto Salinas <TorqueG88@yahoo.com>
2022-06-21 22:46:21 -0400

    codewars

---
+++ my-projects/code-wars/6kyu/playing-with-digits-in-progress/js/main.js
@@ -0,0 +1,34 @@
+// Instructions
+
+// Some numbers have funny properties. For example:
+
+// 89 --> 8¹ + 9² = 89 * 1
+
+// 695 --> 6² + 9³ + 5⁴= 1390 = 695 * 2
+
+// 46288 --> 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
+
+// Given a positive integer n written as abcd... (a, b, c, d... being digits) and a positive integer p
+
+// we want to find a positive integer k, if it exists, such that the sum of the digits of n taken to the successive powers of p is equal to k * n.
+// In other words:
+
+// Is there an integer k such as : (a ^ p + b ^ (p+1) + c ^(p+2) + d ^ (p+3) + ...) = n * k
+
+// If it is the case we will return k, if not return -1.
+
+// Note: n and p will always be given as strictly positive integers.
+
+function digPow(n, p){
+    result=0
+    sum=0
+    nString=n.toString()
+    for (let x in nString){
+        sum+=Math.pow(Number(nString[x]), p)
+        p++
+    }  if (sum%n===0){
+        result=sum/n
+    }else{
+        result=-1
+    }return(result)
+}
