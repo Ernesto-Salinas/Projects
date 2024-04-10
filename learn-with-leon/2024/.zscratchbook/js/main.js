0ee6c11f414465ccb7f6245bbc01f0fa1e88cb5d
Ernesto Salinas <TorqueG88@yahoo.com>
2024-04-10 01:52:16 -0400

    Finish class 2

---
+++ learn-with-leon/2024/.zscratchbook/js/main.js
@@ -0,0 +1,11 @@
+// Class 20 - Arrays
+
+let bestColors = ['green','blue','yellow','black']
+
+bestColors.forEach((x,i)=> console.log(x))
+
+//or
+
+/* for(let i = 0; i < bestColors.length;i++){
+    console.log( bestColors[i] )
+  } */
