4c1a058f7d09ba5f8c194480064d4119b2cdfe68
Ernesto Salinas <TorqueG88@yahoo.com>
2025-10-28 02:41:16 -0400

    Code Wars: Python
    2 Kyus completed

---
+++ my-projects/code-wars/6kyu/sum-of-digits_digital-root/python.py
@@ -0,0 +1,26 @@
+"""
+Digital root is the recursive sum of all the digits in a number.
+
+Given n, take the sum of the digits of n. If that value has more than one digit, continue reducing in this way until a single-digit number is produced. The input will be a non-negative integer.
+
+Examples
+    16  -->  1 + 6 = 7
+   942  -->  9 + 4 + 2 = 15  -->  1 + 5 = 6
+132189  -->  1 + 3 + 2 + 1 + 8 + 9 = 24  -->  2 + 4 = 6
+493193  -->  4 + 9 + 3 + 1 + 9 + 3 = 29  -->  2 + 9 = 11
+
+def digital_root(n):
+    pass # your code here
+"""
+def digital_root(n):
+    ans=10
+    while ans >= 10:
+        result=0
+        for x in str(n):
+            result+=int(x)
+        n=result
+        ans=result
+    print(ans)
+
+
+digital_root(132189)
